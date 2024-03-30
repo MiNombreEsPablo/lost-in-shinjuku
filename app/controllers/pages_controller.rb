2fdb642b92a7f68ddc923fa95ebda8734351980c
Pablo Bugueno <pablo.buguenoe@gmail.com>
2024-03-30 12:52:31 +0900

    page controller and index

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,10 @@
 # frozen_string_literal: true
 
 class PagesController < ApplicationController
-  def index; end
+  def index
+    @image = 'lzmoyshjcs8jlimoxxab'
+    @phase = 0
+    @story = "You are a foreigner in Shinjuku station, and you're late for your date."
+    @options = { '1': 'Start' }
+  end
 end
--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,8 @@
 //= require jquery3
 //= require popper
 //= require bootstrap
+
+// Entry point for the build script in your package.json
+import "@hotwired/turbo-rails"
+import "./controllers"
+// import "bootstrap"
